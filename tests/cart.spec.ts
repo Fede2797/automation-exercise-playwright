61b2d824d09bb854f223d950f6ba6432145f3e18
Fede <federico2797@gmail.com>
2023-09-01 11:56:55 -0300

    Finished Cart section testing

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { URL, homepageVisible } from "./helpers/helper";
+import { URL, handleMultipleGoogleAds, homepageVisible } from "./helpers/helper";
 import { enterProductsPage } from "./helpers/products-helper";
 
 test.beforeEach(async({ page }) => {
@@ -7,7 +7,7 @@ test.beforeEach(async({ page }) => {
 });
 
 test.describe("Testing Cart functionallity", () => {
-  test.only("Test Case 12: Add Products in Cart", async({ page }) => {
+  test("Test Case 12: Add Products in Cart", async({ page }) => {
     await homepageVisible( page );
     await enterProductsPage( page );
     const nameFirstProduct = await page.locator("xpath=/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/p").textContent();
@@ -15,11 +15,9 @@ test.describe("Testing Cart functionallity", () => {
     const nameSecondProduct = await page.locator("xpath=/html/body/section[2]/div/div/div[2]/div/div[3]/div/div[1]/div[1]/p").textContent();
     const priceSecondProduct = await page.locator("xpath=/html/body/section[2]/div/div/div[2]/div/div[3]/div/div[1]/div[1]/h2").textContent();
     await page.locator(".single-products").first().hover();
-    // await page.locator('.overlay-content > .btn').first().click();
     await page.locator('.productinfo > .btn').first().click();
     await page.getByRole('button', { name: 'Continue Shopping' }).click();
     await page.locator(".single-products").nth(1).hover();
-    // await page.locator(".single-products").nth(1).getByText("Add to cart").first().click();
     await page.locator('div:nth-child(4) > .product-image-wrapper > .single-products > .productinfo > .btn').first().click();
     await page.getByRole('link', { name: 'View Cart' }).click();
     
@@ -43,5 +41,24 @@ test.describe("Testing Cart functionallity", () => {
       await expect(secondProductQuantity).toBe((quantity).toString());
       await expect(secondProductTotalPrice).toBe(priceSecondProduct);
     }
+  })
+
+  test.only("Test Case 13: Verify Product quantity in Cart", async({ page }) => {
+    const quantity = 4;
+
+    await homepageVisible( page );
+    await page.locator('div:nth-child(7)').getByText("View Product").click();
+    await handleMultipleGoogleAds( page );
+    await expect(page).toHaveURL(/product_details/);
+    await page.locator('#quantity').fill((quantity).toString());
+
+    const productName = await page.locator(".product-information > h2").textContent();
+    const productPrice = await page.locator(".product-information > span > span").textContent();
+
+    await page.getByRole('button', { name: 'Add to cart' }).click();
+    await page.getByRole('link', { name: 'View Cart' }).click();
+    await expect( await page.locator("tbody > tr:nth-child(1) > td:nth-child(2) > h4").textContent() ).toBe(productName);
+    await expect( await page.locator("tbody > tr:nth-child(1) > td:nth-child(3) > p").textContent() ).toBe(productPrice);
+    await expect( await page.locator("tbody > tr:nth-child(1) > td:nth-child(4) > button").textContent() ).toBe((quantity).toString());
   })
 })
